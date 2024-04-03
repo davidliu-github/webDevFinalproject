941b5baf0f51bc131a8cc97a76f601a3aec1b26c
jasongao07 <gao.ja@northeastern.edu>
2024-04-03 00:13:10 -0400

    a5

--- src/Kanbas/Courses/Modules/client.ts
+++ src/Kanbas/Courses/Modules/client.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const COURSES_API = 'http://localhost:4000/api/courses'
+const API_BASE = process.env.REACT_APP_API_BASE
+const COURSES_API = `${API_BASE}/api/courses`
 export const findModulesForCourse = async (courseId: any) => {
   const response = await axios.get(`${COURSES_API}/${courseId}/modules`)
   return response.data
@@ -10,7 +11,7 @@ export const createModule = async (courseId: any, module: any) => {
   return response.data
 }
 
-const MODULES_API = 'http://localhost:4000/api/modules'
+const MODULES_API = `${API_BASE}/api/modules`
 export const deleteModule = async (moduleId: any) => {
   const response = await axios.delete(`${MODULES_API}/${moduleId}`)
   return response.data
