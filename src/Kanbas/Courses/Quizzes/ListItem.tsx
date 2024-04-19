4e2898eeb8074b7430e7e1b51bb4686fb1cba411
davidliu-github <davlui7402@gmail.com>
2024-04-19 00:12:41 -0400

    temp

--- src/Kanbas/Courses/Quizzes/ListItem.tsx
+++ src/Kanbas/Courses/Quizzes/ListItem.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { FaCheckCircle, FaEllipsisV, FaPlusCircle, FaBan, FaRocket } from 'react-icons/fa';
 import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 type QuizTemplate = {
   quizType: string;
@@ -38,7 +38,7 @@ type Test = {
 
 const ListItem: React.FC<Quiz> = (props) => {
   const { pathname } = useLocation();
-  const [showPopup, setShowPopup] = React.useState(false);
+  const navigate = useNavigate();
   const { availableDate, dueDate, points, questions, published, title } = props.quiz;
   return (
     <li className="list-group-item mt-2">
@@ -49,7 +49,7 @@ const ListItem: React.FC<Quiz> = (props) => {
         
         <Menu menuButton={<MenuButton style={{ "backgroundColor": 'white' }}><FaEllipsisV /></MenuButton>}>
           <div style={{ "backgroundColor": "white", "position": "fixed", "zIndex": 100 }}>
-            <MenuItem style={{ "listStyleType": "none" }}><Link to={pathname + "/" + props.quiz.title + "/edit"}>Edit</Link></MenuItem>
+            <MenuItem style={{ "listStyleType": "none" }} onClick={()=>{navigate(pathname + '/' + props.quiz.title + '/edit', {state:{quiz: props.quiz}})}}>Edit</MenuItem>
             <MenuItem style={{ "listStyleType": "none" }} onClick={props.deleteQuiz}>Delete</MenuItem>
             <MenuItem style={{ "listStyleType": "none" }}>Publish</MenuItem>
           </div>
--- src/Kanbas/Courses/Quizzes/QuizEdit.tsx
+++ src/Kanbas/Courses/Quizzes/QuizEdit.tsx
@@ -1,8 +1,170 @@
-function QuizEdit(){
+import React, { useState } from "react";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 
+type InputEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement>;
+
+
+function QuizEdit(){
+    const navigation = useNavigate();
+
+    const location = useLocation();
+    const [titleInputValue, setTitleInputValue] = useState(location.state.quiz.title);
+    const [quizType, setQuizType] = useState(location.state.quiz.quizType);
+    const [points, setPoints] = useState(location.state.quiz.points);
+    const [assignmentGroup, setAssignmentGroup] = useState(location.state.quiz.assignmentGroup);
+    const [shuffleAnswers, setShuffleAnswers] = useState(location.state.quiz.shuffleAnswers);
+    const [timeLimit, setTimeLimit] = useState(location.state.quiz.timeLimit);
+    const [multipleAttempts, setMultipleAttempts] = useState(location.state.quiz.multipleAttempts);
+    const [showCorrectAnswers, setShowCorrectAnswers] = useState(location.state.quiz.showCorrectAnswers);
+    const [accessCode, setAccessCode] = useState(location.state.quiz.accessCode);
+    const [oneQuestionAtATime, setOneQuestionAtATime] = useState(location.state.quiz.oneQuestionAtATime);
+    const [webCamRequired, setWebCamRequired] = useState(location.state.quiz.webCamRequired);
+    const [lockQuestion, setLockQuestion] = useState(location.state.quiz.lockQuestion);
+    const [dueDate, setDueDate] = useState(location.state.quiz.dueDate);
+    const [availableDate, setAvailableDate] = useState(location.state.quiz.availableDate);
+    const [untilDate, setUntilDate] = useState(location.state.quiz.untilDate);
+    const [published, setPublished] = useState(location.state.quiz.published);
+
+    const handleChange = (
+      e: InputEvent,
+      setValue: React.Dispatch<React.SetStateAction<string>>
+    ) => {
+      setValue(e.target.value);
+    };
+
+    const handleChangeCheckBox = (
+      e: React.ChangeEvent<HTMLInputElement>,
+      setValue: React.Dispatch<React.SetStateAction<boolean>>
+    ) => {
+      setValue(e.target.checked);
+    };
+
+    const handleCancel = () => {
+      navigation(-1);
+    };
+
+    console.log(location);
     return (
       <div>
-        {/* Component JSX goes here */}
+        Points {location.state.quiz.points}
+        {console.log(location.state.quiz.points)}
+        {location.state.published ? "Published" : "Not Published"}
+        <nav className="nav nav-tabs mt-2">
+              <Link className="nav-link active" to="/Quizzes/blank/edit">Details</Link>
+              <Link style={{"color": "red"}} className="nav-link" to="/Labs/a3">Questions</Link>
+        </nav>
+        <label htmlFor="input1"
+         className="form-label">
+    Title</label>
+    <input type="text"
+         className="form-control"
+         id="input1"
+         value={titleInputValue} onChange={(e) => handleChange(e,setTitleInputValue)}/>
+
+<label>Quiz Type</label>
+      <select value={quizType} onChange={(e) => handleChange(e,setQuizType)}>
+        <option value="GRADED_QUIZ">Graded Quiz</option>
+        <option value="PRACTICE_QUIZ">Practice Quiz</option>
+        <option value="GRADED_SURVEY">Graded Survey</option>
+        <option value="UNGRADED_SURVEY">Ungraded Survey</option>
+      </select>
+
+      <label>Points</label>
+      <input
+      type="number"
+      value={points}
+      onChange={(e) => handleChange(e,setPoints)}
+    />
+
+<label>Assignment Group</label>
+      <select value={assignmentGroup} onChange={(e) => handleChange(e,setAssignmentGroup)}>
+        <option value="Quizzes">Quizzes</option>
+        <option value="Exams">Exams</option>
+        <option value="Assignments">Assignments</option>
+        <option value="Project">Project</option>
+
+      </select>
+      <label>Shuffle Answers</label>
+      <input
+        type="checkbox"
+        checked={shuffleAnswers}
+        onChange={(e) => handleChangeCheckBox(e,setShuffleAnswers)}
+      />
+
+<label>Time Limit</label>
+      <input
+      type="number"
+      value={timeLimit}
+      onChange={(e) => handleChange(e,setTimeLimit)}
+    />
+
+<label>Multiple Attempts</label>
+      <input
+        type="checkbox"
+        checked={multipleAttempts}
+        onChange={(e) => handleChangeCheckBox(e,setMultipleAttempts)}
+      />
+
+<label>Show Correct Answers</label>
+      <input
+        type="checkbox"
+        checked={showCorrectAnswers}
+        onChange={(e) => handleChangeCheckBox(e,setShowCorrectAnswers)}
+      />
+
+<label>
+    Access Code</label>
+    <input type="text"
+         className="form-control"
+         id="input1"
+         value={accessCode} onChange={(e) => handleChange(e,setAccessCode)}/>
+
+<label>One Question At A Time</label>
+      <input
+        type="checkbox"
+        checked={oneQuestionAtATime}
+        onChange={(e) => handleChangeCheckBox(e,setOneQuestionAtATime)}
+      />
+
+<label>Webcam Required</label>
+      <input
+        type="checkbox"
+        checked={webCamRequired}
+        onChange={(e) => handleChangeCheckBox(e,setWebCamRequired)}
+      />
+
+<label>Lock Question</label>
+      <input
+        type="checkbox"
+        checked={lockQuestion}
+        onChange={(e) => handleChangeCheckBox(e,setLockQuestion)}
+      />
+
+      <label>Due Date</label>
+      <input
+      type="date"
+      value={dueDate}
+      onChange={(e) => handleChange(e,setDueDate)}
+    />
+
+<label>Available Date</label>
+      <input
+      type="date"
+      value={availableDate}
+      onChange={(e) => handleChange(e,setAvailableDate)}
+    />
+
+<label>Until Date</label>
+      <input
+      type="date"
+      value={untilDate}
+      onChange={(e) => handleChange(e,setUntilDate)}
+    />
+
+    <button onClick={handleCancel}>Cancel</button>
+    <button>Save and Publish</button>
+    <button>Save</button>
+
       </div>
     );
   };
